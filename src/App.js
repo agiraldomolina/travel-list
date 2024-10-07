4d0af047e47a1517331ddb0ac9f3949ba0001c23
Alba Giraldo <agiraldomolina@gmail.com>
2024-10-07 07:43:32 -0400

    Lifting State Up

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item){
+    setItems(items=>[...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems}/>
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +27,7 @@ export default function App() {
 function Logo(){
   return <h1>🌴 Far Away 🎒</h1>
 }
-function Form(){
+function Form({ onAddItems}){
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
   
@@ -31,6 +37,8 @@ function Form(){
     if(!description) return;
 
     const newItem = {description, quantity, packed: false, id: Date.now()};
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -58,11 +66,11 @@ function Form(){
     </form>
   )
 }
-function PackingList(){
+function PackingList({items}){
   return (
     <div className="list">
       <ul >
-        {initialItems.map((item)=>
+        {items.map((item)=>
           <Item item={item} key={item.id}/>
           )} 
       </ul>
